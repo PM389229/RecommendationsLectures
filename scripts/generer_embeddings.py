c914bd67a707536a484e9945b548190aa77a5308
PM389229 <pmguevel29@gmail.com>
2024-10-29 16:20:58 +0100

    mmodifs

--- scripts/generer_embeddings.py
+++ scripts/generer_embeddings.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 # Charger les données et le modèle
-data = pd.read_csv('data/final_dataset_clean.csv')
+data = pd.read_csv('StockDatas/final_dataset_clean.csv')
 model = SentenceTransformer('all-MiniLM-L6-v2')
 descriptions = data['description'].tolist()
 
